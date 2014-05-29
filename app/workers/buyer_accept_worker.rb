6b3f8d87b462f17cf5042153000bb0a6042bb754
daveyk535 <dave@phase2minds.com>
2014-05-29 12:22:44 -0700

    record deleted files

--- app/workers/buyer_accept_worker.rb
+++
@@ -1,8 +0,0 @@
-class BuyerAcceptWorker
-   include Sidekiq::Worker
-
-  def perform(email, name)
-    BeeThreeMailer.buyer_offer(email, name).deliver
-  end
-
-end
--- app/workers/email_worker.rb
+++
@@ -1,8 +0,0 @@
-class EmailWorker
-   include Sidekiq::Worker
-
-  def perform(email, name)
-    BeeThreeMailer.buyer_offer(email, name).deliver
-  end
-
-end
